c1282558b8a2baaf5712e707de31ee9dafc22c93
grenada98 <alextod988@gmail.com>
2023-02-01 23:45:10 +0200

    edit css

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -14,28 +14,33 @@ export const CategoryPage = (props) => {
     const dataEnlingo = JSON.parse(localStorage.getItem('enlingo'));
 
     return (
-        <div className={props.activeCategoryPage ? 'categories-page active' : 'categories-page'}>
-            <div className='route-pages'>
-                <Link className='statistics' 
-                    onClick={props.setStatisticsPageActive}
-                    to="/statistics"
-                    >Статистика</Link>
-                <div className='add-words' onClick={props.setNewWordPageActive}>Додати слова</div>
-            </div>
-            <div className='title'>Оберіть категорію</div>
-            <CategoryList 
-                dataEnlingo={dataEnlingo}
-                setEmpty={setEmpty} 
-                img={category_img} 
-                categorySelected={categorySelected} 
-                setCategory={setCategorySelected} 
-                setWords={setWordsSelected}/>
-            {categorySelected.length>0&&wordsSelected.length>0? <WordPanel words={wordsSelected} empty={empty} setEmpty={setEmpty} />: null}
-            {categorySelected.length>0&&wordsSelected.length<1 ?<div className='categories-page__no-words'>В цій категорії ще не додано слів!</div>: null}
-            <Routes>
-                <Route path="/statistics" element={<Statistics/>}/>
-                <Route path="/add-words" element={<NewWord/>}/>
-            </Routes>
-        </div>
+        <>
+            {props.activeCategoryPage?
+                <div className='categories-page'>
+                    <div className='route-pages'>
+                        <Link className='statistics' 
+                            onClick={props.setStatisticsPageActive}
+                            to="/statistics"
+                            >Статистика</Link>
+                        <div className='add-words' onClick={props.setNewWordPageActive}>Додати слова</div>
+                    </div>
+                    <span className='title'>Оберіть категорію</span>
+                    <CategoryList 
+                        dataEnlingo={dataEnlingo}
+                        setEmpty={setEmpty} 
+                        img={category_img} 
+                        categorySelected={categorySelected} 
+                        setCategory={setCategorySelected} 
+                        setWords={setWordsSelected}/>
+                    {categorySelected.length>0&&wordsSelected.length>0? <WordPanel words={wordsSelected} empty={empty} setEmpty={setEmpty} />: null}
+                    {categorySelected.length>0&&wordsSelected.length<1 ?<div className='categories-page__no-words'>В цій категорії ще не додано слів!</div>: null}
+                    <Routes>
+                        <Route path="/statistics" element={<Statistics/>}/>
+                        <Route path="/add-words" element={<NewWord/>}/>
+                    </Routes>
+                </div>
+                : null
+            }
+        </>
     )
 }
--- src/components/NewWord.js
+++ src/components/NewWord.js
@@ -28,8 +28,8 @@ export const NewWord = (props) => {
     }, [categorySelected])
     function addNewWord(){
         const new_obj = {
-            eng: engWordInput.current.value,
-            translates: ukrWordInput.current.value.split()
+            eng: engWordInput.current.value.toLowerCase(),
+            translates: ukrWordInput.current.value.toLowerCase().split()
         }
         dataCategory[indexCategory].words.push(new_obj);
         setArrayWord([...dataCategory[indexCategory].words]);
@@ -45,61 +45,64 @@ export const NewWord = (props) => {
         props.setActiveCategoryPage(true);
     }
     return(
-        <div className={props.activeNewWord? "adding__wrapper active": "adding__wrapper"}>
-            <div className='route-pages'>
-                <Link className='to-main' to={'/'} onClick={backToMain}>На головну</Link>
-            </div>
-            <div className='title'>Оберіть категорію для додавання слів</div>
-            <div className='categories-wrapper'>
-                {
-                    props.dataEnlingo.map(function (key, index) {
-                        return (
-                            <Category key={index} 
-                            setEmpty={setEmpty} 
-                            category={key} 
-                            img={category_img[key.category_name]} 
-                            categorySelected={categorySelected} 
-                            setCategory={setCategorySelected} 
-                            setWords={setWordsSelected} />
-                        )
-                    })
-                }
-            </div>
-            {categorySelected?
-                <div>
-                    <div className='newword__form'>
-                        <input ref={engWordInput} className="newword__input" placeholder='Додайте нове слово на англійській мові'></input>
-                        <input ref={ukrWordInput} className="newword__input" placeholder='Додайте переклад цього слова на українській мові або декілька слів-синонімів'></input>
-                        <button className='newword__button' onClick={addNewWord}>Додати</button>
-                    </div>
-                    {arrayWord?
-                        <>
-                            <table className='vocabulary__table-wrapper'>
-                                <thead>
-                                    <tr className='vocabulary__row'>
-                                        <th className='vocabulary__eng'>Слово</th>
-                                        <th className='vocabulary__ukr'>Переклад</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {arrayWord?.map((item, index)=>{
-                                        return(
-                                            <tr key={index} className='vocabulary__row'>
-                                                <td className='vocabulary__word'>{item.eng}</td>
-                                                <td className='vocabulary__right --newword-table'>{item.translates.length>1? item.translates.join(', '): item.translates}
-                                                <Basket className="vocabulary__delete" onClick={()=>deleteWord(index)}/></td>
-                                            </tr>
-                                        )
-                                    })
-                                    }
-                                </tbody>
-                            </table>
-                            <div className='title'>Всього в категорії {arrayWord.length} слів (слово)</div>
-                        </>
-                        : null}
+        <>
+            {props.activeNewWord?
+            <div className="adding__wrapper">
+                <div className='route-pages'>
+                    <Link className='to-main' to={'/'} onClick={backToMain}>На головну</Link>
                 </div>
-                : null
-            }
-        </div>
+                <span className='title'>Оберіть категорію для додавання слів</span>
+                <div className='categories-wrapper'>
+                    {
+                        props.dataEnlingo.map(function (key, index) {
+                            return (
+                                <Category key={index} 
+                                setEmpty={setEmpty} 
+                                category={key} 
+                                img={category_img[key.category_name]} 
+                                categorySelected={categorySelected} 
+                                setCategory={setCategorySelected} 
+                                setWords={setWordsSelected} />
+                            )
+                        })
+                    }
+                </div>
+                {categorySelected?
+                    <div className='newword__wrapper'>
+                        <div className='newword__form'>
+                            <input ref={engWordInput} className="newword__input" placeholder='Додайте нове слово на англійській мові'></input>
+                            <input ref={ukrWordInput} className="newword__input" placeholder='Додайте переклад цього слова на українській мові або декілька слів-синонімів'></input>
+                            <button className='newword__button' onClick={addNewWord}>Додати</button>
+                        </div>
+                        {arrayWord.length?
+                            <>
+                                <table className='vocabulary__table-wrapper'>
+                                    <thead>
+                                        <tr className='vocabulary__row'>
+                                            <th className='vocabulary__eng'>Слово</th>
+                                            <th className='vocabulary__ukr'>Переклад</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {arrayWord.map((item, index)=>{
+                                            return(
+                                                <tr key={index} className='vocabulary__row'>
+                                                    <td className='vocabulary__word'>{item.eng}</td>
+                                                    <td className='vocabulary__right'>{item.translates.length>1? item.translates.join(', '): item.translates}
+                                                    <Basket className="vocabulary__delete" onClick={()=>deleteWord(index)}/></td>
+                                                </tr>
+                                            )
+                                        })
+                                        }
+                                    </tbody>
+                                </table>
+                                <div className='title'>Всього в категорії {arrayWord.length} слів (слово)</div>
+                            </>
+                            : null}
+                    </div>
+                    : null
+                }
+            </div> : null}
+        </>
     )
 }
